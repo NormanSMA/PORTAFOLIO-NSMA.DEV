cbd2743d5b1dc5f50b359ba6c4ef38bc27fca000
Norman <normanma8@gmail.com>
2025-11-10 13:06:20 -0600

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
-import { Navbar, Hero, About, Services, Footer, Projects } from './components/';
+import { Navbar, Hero, About, Services, Footer, Projects } from './components/organisms';
 import { ScrollToTop } from './components/molecules';
 import { Analytics } from '@vercel/analytics/react';
 
